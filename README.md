20165810fbf79e480bf2cee63b7d3d7dad89d7c5
ricky <knroro1877887@gmail.com>
2020-08-28 10:39:25 +0900

    Fixed html file

---
+++ README.md
@@ -0,0 +1,2 @@
+This app conveys information by voice.
+For example pdf, web site news, twitter tweet.
---
+++ mysite/audio_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AudioSystemConfig(AppConfig):
+    name = 'audio_system'
